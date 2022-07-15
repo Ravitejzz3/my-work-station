cb0bdd30c2c8861f65e8bcf3850c15351ffafe2e
Ravi Teja <ravitejzz380@gmail.com>
2022-07-15 21:06:17 +0530

    update the first line

--- 01-variable.sh
+++ 01-variable.sh
@@ -5,3 +5,5 @@ echo "Good morning, todays date is $DATE_COMMAND"
 
 echo "total no of sessions are $(who |wc-1)"
 echo "the running file is $0"
+echo -e "\e[32m Iam in visual studio code\e[0m"
+echo -e "\e[40;32m Iam printing colours \e[0m"
